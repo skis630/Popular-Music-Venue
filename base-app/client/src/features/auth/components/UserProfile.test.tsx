6fecbefa0a0e3a99cddc2c3213d3a723747bbb84
skis630 <skis630@aucklanduni.ac.nz>
2022-09-02 16:53:34 +0300

    Fix linting on test file

--- base-app/client/src/features/auth/components/UserProfile.test.tsx
+++ base-app/client/src/features/auth/components/UserProfile.test.tsx
@@ -1,19 +1,23 @@
-import {render, screen} from "../../../test-utils";
+import { render, screen } from "../../../test-utils";
 import { UserProfile } from "./UserProfile";
 
 const testUser = {
-    email: "booking@avalancheofcheese.com"
-}
+  email: "booking@avalancheofcheese.com",
+};
 
 test("greets the user", () => {
-    render(<UserProfile />, {preLoadedState: { user: {userDetails: testUser}}});
+  render(<UserProfile />, {
+    preLoadedState: { user: { userDetails: testUser } },
+  });
 
-    expect(screen.getByText(/hi, booking@avalancheofcheese.com/i)).toBeInTheDocument();
+  expect(
+    screen.getByText(/hi, booking@avalancheofcheese.com/i)
+  ).toBeInTheDocument();
 });
 
 test("redirects to /signin if user is falsy", () => {
-    const {history} = render(<UserProfile />);
+  const { history } = render(<UserProfile />);
 
-    expect(screen.queryByText(/hi/i)).not.toBeInTheDocument();
-    expect(history.location.pathname).toBe("/signin");
-})
+  expect(screen.queryByText(/hi/i)).not.toBeInTheDocument();
+  expect(history.location.pathname).toBe("/signin");
+});
