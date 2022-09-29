832f3ebd37618c75d143590dcd47fe0ad4a8787d
skis630 <skis630@aucklanduni.ac.nz>
2022-09-30 01:18:51 +0300

    Fix typo

--- base-app/client/src/mocks/server.js
+++ base-app/client/src/mocks/server.js
@@ -1,6 +1,6 @@
-import { setupSever } from "msw/node";
+import { setupServer } from "msw/node";
 
 import { handlers } from "./handlers";
 
 // This configures a request mocking server with the given requests
-export const server = setupSever(...handlers);
+export const server = setupServer(...handlers);
