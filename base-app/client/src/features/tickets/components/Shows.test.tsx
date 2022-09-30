a5b2ceb0fb0d2ab8e965f7c3b19a3c711617791a
skis630 <skis630@aucklanduni.ac.nz>
2022-09-30 15:11:34 +0300

    Code quiz: msw for sold out show

--- base-app/client/src/features/tickets/components/Shows.test.tsx
+++ base-app/client/src/features/tickets/components/Shows.test.tsx
@@ -19,3 +19,17 @@ test("displays relevant details for non-sold-out show", async () => {
   const bandDescription = getByText(nonSoldOutShow, "rollicking country with ambitious kazoo solos");
   expect(bandDescription).toBeInTheDocument()
 });
+
+test("displays relevant details for sold out show", async () => {
+    render(<Shows />);
+    const shows = await screen.findAllByRole("listitem");
+    const soldOutShow = shows[1];
+    const soldOutMsg = getByRole(soldOutShow, "heading", {
+        name: /sold out/i,
+    });
+    expect(soldOutMsg).toBeInTheDocument();
+    const bandName = getByRole(soldOutShow, "heading", { name: /The Joyous Nun Riot/i });
+    expect(bandName).toBeInTheDocument();
+    const bandDescription = getByText(soldOutShow, "serious world music with an iconic musical saw");
+    expect(bandDescription).toBeInTheDocument();
+})
