7806058adc717c419111bfaa94736726e41d7526
skis630 <skis630@aucklanduni.ac.nz>
2022-10-01 23:17:10 +0300

    Add handler for band page request

--- base-app/client/src/mocks/handlers.js
+++ base-app/client/src/mocks/handlers.js
@@ -1,10 +1,17 @@
 import { rest } from "msw";
 
 import { showsUrl } from "../features/tickets/redux/showApi";
-import { shows } from "../test-utils/fake-data";
+import { bandUrl } from "../features/band/redux/bandApi";
+import { bands, shows } from "../test-utils/fake-data";
 
 export const handlers = [
   rest.get(showsUrl, (req, res, ctx) => {
-    return ctx.json({ shows });
+    return res(ctx.json({ shows }));
   }),
+  rest.get(`${bandUrl}:bandId`, (req, res, ctx) => {
+    const { bandId } = req.params;
+
+    // bandId is conveniently the index in the bands array
+    return res(ctx.json({ band: bands[bandId] }))
+  })
 ];
