b304543bf099fbdfe06df861c67e978e4d94b658
skis630 <skis630@aucklanduni.ac.nz>
2022-10-10 22:40:50 +0300

    Add all protected routes to parametrized test

--- base-app/client/src/features/auth/tests/protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/protected-routes.test.tsx
@@ -2,12 +2,14 @@ import { App } from "../../../App";
 import { render, screen } from "../../../test-utils/index";
 
 test.each([
-  { routeName: "Profile", routePath: "/profile", headingMatch: /sign in/i },
+  { route: "/profile" },
+  { route: "/tickets/0" },
+  { route: "/confirm/0?holdId=123&seatCount=2" },
 ])(
-  "test redirect to sign-in if user is not logged in to $routeName",
-  ({ routePath, headingMatch }) => {
-    render(<App />, { routeHistory: [routePath] });
-    const heading = screen.getByRole("heading", { name: headingMatch });
-    expect(heading).toBeInTheDocument();
+  "test redirect to sign-in if user is not logged in to $route",
+  ({ route }) => {
+    render(<App />, { routeHistory: [route] });
+    const signInHeader = screen.getByRole("heading", { name: /sign in/i });
+    expect(signInHeader).toBeInTheDocument();
   }
 );
