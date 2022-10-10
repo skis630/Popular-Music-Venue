280fe22fb06ba3131b9891f4d33f7c89622552ff
skis630 <skis630@aucklanduni.ac.nz>
2022-10-11 01:25:38 +0300

    Remove unnecessary tests

--- base-app/client/src/features/auth/tests/protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/protected-routes.test.tsx
@@ -16,67 +16,25 @@ test.each([
   }
 );
 
-test("successful sign-in flow", async () => {
-  // go to protected page
+test.each([
+  { testName: "sign in", buttonName: /sign in/i },
+  { testName: "sign up", buttonName: /sign up/i },
+])("successful $testName flow", async ({ buttonName, testName }) => {
   const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
 
-  // Sign in (after redirect)
+  // Sign in/sign up after redirect
   const emailField = screen.getByLabelText(/email/i);
   userEvent.type(emailField, "booking@avalancheofcheese.com");
 
   const passwordField = screen.getByLabelText(/password/i);
   userEvent.type(passwordField, "iheartcheese");
 
-  const signInForm = screen.getByTestId("sign-in-form");
-  const signInButton = getByRole(signInForm, "button", { name: /sign in/i });
-  userEvent.click(signInButton);
-
-  await waitFor(() => {
-    // test redirect back to protected page
-    expect(history.location.pathname).toBe("/tickets/1");
-    // sign-in page removed from history
-    expect(history.entries).toHaveLength(1);
-  });
-});
-
-test("successful sign-up flow", async () => {
-  const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
-
-  // Sign up (after redirect)
-  const emailField = screen.getByLabelText(/email/i);
-  userEvent.type(emailField, "booking@avalancheofcheese.com");
-
-  const passwordField = screen.getByLabelText(/password/i);
-  userEvent.type(passwordField, "iheartcheese");
-
-  const signUpButton = screen.getByRole("button", { name: /sign up/i });
-  userEvent.click(signUpButton);
+  const authForm = screen.getByTestId("sign-in-form");
+  const authButton = getByRole(authForm, "button", { name: buttonName });
+  userEvent.click(authButton);
 
   await waitFor(() => {
     expect(history.location.pathname).toBe("/tickets/1");
     expect(history.entries).toHaveLength(1);
   });
 });
-
-test.each([{ name: /sign in/i }, { name: /sign up/i }])(
-  "successful sign-in/sign-up flow",
-  async ({ name }) => {
-    const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
-
-    // Sign in/sign up after redirect
-    const emailField = screen.getByLabelText(/email/i);
-    userEvent.type(emailField, "booking@avalancheofcheese.com");
-
-    const passwordField = screen.getByLabelText(/password/i);
-    userEvent.type(passwordField, "iheartcheese");
-
-    const authForm = screen.getByTestId("sign-in-form");
-    const authButton = getByRole(authForm, "button", { name });
-    userEvent.click(authButton);
-
-    await waitFor(() => {
-      expect(history.location.pathname).toBe("/tickets/1");
-      expect(history.entries).toHaveLength(1);
-    });
-  }
-);
