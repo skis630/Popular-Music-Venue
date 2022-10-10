a163c956ce025dd93caad4995ebea3ce8d5cf01e
skis630 <skis630@aucklanduni.ac.nz>
2022-10-10 23:06:07 +0300

    Add handlers for sign-in & sign-out routes

--- base-app/client/src/mocks/handlers.js
+++ base-app/client/src/mocks/handlers.js
@@ -3,6 +3,19 @@ import { rest } from "msw";
 import { showsUrl } from "../features/tickets/redux/showApi";
 import { bandUrl } from "../features/band/redux/bandApi";
 import { bands, shows } from "../test-utils/fake-data";
+import { baseUrl, endpoints } from "../app/axios/constants";
+
+const authHandler = (req, res, ctx) => {
+  const { email } = req.body;
+
+  return res(ctx.json({
+    user: {
+      id: 123,
+      email,
+      token: "abc123"
+    }
+  }))
+}
 
 export const handlers = [
   rest.get(showsUrl, (req, res, ctx) => {
@@ -22,5 +35,7 @@ export const handlers = [
   }),
   rest.patch(`${showsUrl}/:showId/hold/:holdId`, (req, res, ctx) => {
     return res(ctx.status(200));
-  })
+  }),
+  rest.post(`${baseUrl}/${endpoints.signIn}`, authHandler),
+  rest.post(`${baseUrl}/${endpoints.signUp}`, authHandler),
 ];
