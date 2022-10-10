c870411a70a4135a9a6e84ef6d07f8566f3f9671
skis630 <skis630@aucklanduni.ac.nz>
2022-10-10 22:31:36 +0300

    Parametrize non-protected routes tests

--- base-app/client/src/features/auth/tests/non-protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/non-protected-routes.test.tsx
@@ -1,9 +1,17 @@
 import { App } from "../../../App";
 import { render, screen } from "../../../test-utils/index";
 
-test("homepage doesn't redirect to sign-in", () => {
-  render(<App />);
-
-  const homepageHeading = screen.getByRole("heading", /welcome/i);
-  expect(homepageHeading).toBeInTheDocument();
-});
+test.each([
+  // can't use object properties in test name until
+  // https://github.com/facebook/jest/pull/11388 is released
+  { routeName: "Home", routePath: "/", headingMatch: /welcome/i },
+  { routeName: "Band 1", routePath: "/bands/1", headingMatch: /Joyous/i },
+  { routeName: "Shows", routePath: "/shows", headingMatch: /upcoming shows/i },
+])(
+  "$routeName page does not redirect to login screen",
+  async ({ routePath, headingMatch }) => {
+    render(<App />, { routeHistory: [routePath] });
+    const heading = await screen.findByRole("heading", { name: headingMatch });
+    expect(heading).toBeInTheDocument();
+  }
+);
