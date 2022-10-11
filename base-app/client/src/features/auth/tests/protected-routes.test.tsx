6a0d46888fdad0ed0e3ab5c558d72e9ab0a6a463
skis630 <skis630@aucklanduni.ac.nz>
2022-10-12 00:03:04 +0300

    Test login failure followed by successful login

--- base-app/client/src/features/auth/tests/protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/protected-routes.test.tsx
@@ -1,6 +1,17 @@
 import userEvent from "@testing-library/user-event";
+import {
+  DefaultRequestBody,
+  RequestParams,
+  ResponseComposition,
+  rest,
+  RestContext,
+  RestRequest,
+} from "msw";
 
 import { App } from "../../../App";
+import { baseUrl, endpoints } from "../../../app/axios/constants";
+import { handlers } from "../../../mocks/handlers";
+import { server } from "../../../mocks/server";
 import { getByRole, render, screen, waitFor } from "../../../test-utils/index";
 
 test.each([
@@ -31,6 +42,41 @@ test.each([
 
   const authForm = screen.getByTestId("sign-in-form");
   const authButton = getByRole(authForm, "button", { name: buttonName });
+  userEvent.click(authButton);
+
+  await waitFor(() => {
+    expect(history.location.pathname).toBe("/tickets/1");
+    expect(history.entries).toHaveLength(1);
+  });
+});
+
+const signInFailure = (
+  req: RestRequest<DefaultRequestBody, RequestParams>,
+  res: ResponseComposition,
+  ctx: RestContext
+) => res(ctx.status(401));
+
+test("unsuccessful login followed by successful login", async () => {
+  const errorHandler = rest.post(
+    `${baseUrl}/${endpoints.signIn}`,
+    signInFailure
+  );
+  server.resetHandlers(...handlers, errorHandler);
+
+  const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
+
+  // Sign in after redirect
+  const emailField = screen.getByLabelText(/email/i);
+  userEvent.type(emailField, "booking@avalancheofcheese.com");
+
+  const passwordField = screen.getByLabelText(/password/i);
+  userEvent.type(passwordField, "iheartcheese");
+
+  const authForm = screen.getByTestId("sign-in-form");
+  const authButton = getByRole(authForm, "button", { name: /sign in/i });
+  userEvent.click(authButton);
+
+  server.resetHandlers();
   userEvent.click(authButton);
 
   await waitFor(() => {
